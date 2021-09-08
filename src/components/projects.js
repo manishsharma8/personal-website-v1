975ab0b8cb4401841c9a9e2c89d949f6f9170517
Manish Sharma <manish.bbps@gmail.com>
2021-09-08 17:24:13 +0530

    project update

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,9 +5,96 @@ import { ExternalLink } from "react-feather"
 const Projects = () => {
   return (
     <div id="work" className="mt-36 md:mt-56">
-      <div className="text-3xl md:text-4xl font-bold">Some Recent Projects</div>
-      <div className="py-11 md:py-24 grid grid-cols-1 lg:grid-cols-2 gap-16">
-        <div className="rounded border-2 border-gray-900 text-sm">
+      <div className="text-3xl md:hidden md:text-4xl font-bold">
+        Some Recent Projects
+      </div>
+      <div className="py-11 md:py-24 grid grid-cols-1 lg:grid-cols-2 grid-rows-6 gap-16">
+        <div className="text-3xl hidden md:block md:text-4xl font-bold">
+          Some Recent Projects
+        </div>
+        <div className="rounded border-2 border-gray-900 text-sm row-span-3">
+          <StaticImage
+            className="rounded md:h-auto"
+            placeholder="blurred"
+            src="../images/pocket-url.png"
+          />
+          <div className="m-3">
+            <div className="flex">
+              <div className="text-xl md:text-2xl font-bold my-4">
+                Pocket Url
+              </div>
+              <a
+                className="flex ml-auto"
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/manishsharma8/Pocket-Url"
+              >
+                <StaticImage
+                  className="ml-auto mr-3 my-auto w-8 h-8"
+                  src="../images/icons8-github.svg"
+                />
+              </a>
+              {/* <a
+                className="flex"
+                target="_blank"
+                rel="noreferrer"
+                href="https://covid19-stats-overview.netlify.app/"
+              >
+                <ExternalLink className="my-auto w-7 h-7 mr-2" />
+              </a> */}
+            </div>
+            <div className="flex flex-wrap gap-3">
+              <div className="pt-1 px-3 rounded opacity-90 bg-blue-900 text-blue-200 font-bold align-middle">
+                <StaticImage
+                  className="w-4 h-4 mr-1 cursor-pointer"
+                  src="../images/typescript.svg"
+                />
+                Typescript
+              </div>
+              <div className="pt-1 px-3 rounded opacity-90 bg-gray-600 text-gray-200 font-bold align-middle">
+                <StaticImage
+                  className="w-4 h-4 mr-1 cursor-pointer"
+                  src="../images/nextjs.svg"
+                />
+                NextJs
+              </div>
+              <div className="pt-1 px-3 rounded opacity-90 bg-green-900 text-green-200 font-bold align-middle">
+                <StaticImage
+                  className="w-4 h-4 mr-1 cursor-pointer"
+                  src="../images/expressjs.svg"
+                />
+                ExpressJs
+              </div>
+              <div className="pt-1 px-3 rounded opacity-90 bg-purple-900 text-purple-200 font-bold align-middle">
+                <StaticImage
+                  className="w-4 h-4 mr-1 cursor-pointer"
+                  src="../images/redis.svg"
+                />
+                Redis
+              </div>
+              <div className="py-1 px-3 rounded opacity-90 bg-pink-900 text-pink-200 font-bold align-middle">
+                <StaticImage
+                  className="w-4 h-4 mr-1 cursor-pointer"
+                  src="../images/graphql.svg"
+                />
+                Graphql
+              </div>
+              <div className="pt-1 px-3 rounded opacity-90 bg-blue-900 text-blue-200 font-bold align-middle">
+                <StaticImage
+                  className="w-4 h-4 mr-1 cursor-pointer"
+                  src="../images/postgresql.svg"
+                />
+                PostgreSQL
+              </div>
+            </div>
+            <div className="my-4 w-full h-0.5 rounded bg-gray-800"></div>
+            <div className="mr-2 text-lg font-medium text-gray-400">
+              A simple and easy URL Shortener. Create short urls with custom
+              aliases and easily track your url visits.
+            </div>
+          </div>
+        </div>
+        <div className="rounded border-2 border-gray-900 text-sm row-span-3">
           <StaticImage
             className="rounded md:h-60"
             placeholder="blurred"
@@ -68,7 +155,7 @@ const Projects = () => {
             </div>
           </div>
         </div>
-        <div className="rounded border-2 border-gray-900 text-sm">
+        <div className="rounded border-2 border-gray-900 text-sm row-span-3">
           <StaticImage
             className="rounded md:h-60"
             placeholder="blurred"
